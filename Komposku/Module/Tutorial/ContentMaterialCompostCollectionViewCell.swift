9393e59cba14926d575f063e43abb48c6882c23f
Fernando Lawrence <68634899+fernandolawrence@users.noreply.github.com>
2021-07-28 20:06:42 +0700

    add tutorial view

---
+++ Komposku/Module/Tutorial/ContentMaterialCompostCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContentMaterialCompostCollectionViewCell.swift
+//  Komposku
+//
+//  Created by Fernando Lawrence on 28/07/21.
+//
+
+import UIKit
+
+class ContentMaterialCompostCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
