d138028a84aeba0f41c473ffb950cf90266559c7
claranggraini <claraanggraini_01@yahoo.com>
2021-07-28 09:40:15 +0700

    initialize core data

---
+++ Komposku/Core/Process+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Process+CoreDataClass.swift
+//  Komposku
+//
+//  Created by Clara Anggraini on 26/07/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Process)
+public class Process: NSManagedObject {
+
+}
