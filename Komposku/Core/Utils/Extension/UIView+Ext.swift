b221bad8dc3f2d86e656406aa1ea622bee138484
Ahmad Zaky W <a.zaky.wisnumurti@mail.ugm.ac.id>
2021-07-29 21:40:43 +0700

    add UIView Extension and create totalitem and button custom components

---
+++ Komposku/Core/Utils/Extension/UIView+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Ext.swift
+//  Komposku
+//
+//  Created by Ahmad Zaky on 29/07/21.
+//
+
+import Foundation
---
+++ Komposku/Module/CreateCompost/CreateButton/CreateButtonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CreateButtonTableViewCell.swift
+//  Komposku
+//
+//  Created by Ahmad Zaky on 29/07/21.
+//
+
+import UIKit
+
+class CreateButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
