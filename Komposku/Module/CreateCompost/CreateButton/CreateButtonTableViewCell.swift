b684e5bbcb4d1857a937d78d72b0173aec769068
Ahmad Zaky W <a.zaky.wisnumurti@mail.ugm.ac.id>
2021-07-29 21:42:47 +0700

    add logic to custom button component

--- Komposku/Module/CreateCompost/CreateButton/CreateButtonTableViewCell.swift
+++ Komposku/Module/CreateCompost/CreateButton/CreateButtonTableViewCell.swift
@@ -9,15 +9,43 @@ import UIKit
 
 class CreateButtonTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var addBahanButton: UIImageView!
+    
+    @IBOutlet weak var containerView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        setupView()
     }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
+    
+    var action: (() -> ())?
+    
+    func setupView() {
+        addBahanButton.layer.cornerRadius = 10
+        createDashPattern()
+        containerView.onTap(action: {
+            self.action?()
+        })
+    }
+    
+    func createDashPattern() {
+        let layer = CAShapeLayer()
+        layer.path = UIBezierPath(roundedRect: addBahanButton.bounds, cornerRadius: 8).cgPath
+        layer.strokeColor = UIColor(red: 0.19, green: 0.59, blue: 0.40, alpha: 1).cgColor
+        layer.fillColor = nil
+        layer.lineWidth = 2
+        layer.lineDashPattern = [8,6]
+        containerView.layer.addSublayer(layer)
+    }
+
     
 }
