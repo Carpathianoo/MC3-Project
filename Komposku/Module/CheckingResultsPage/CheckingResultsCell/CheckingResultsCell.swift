9e1a67e638484d7a3c3526f90588616aef9a9cce
Aufar <adlanaufar@gmail.com>
2021-08-03 11:36:51 +0700

    add checking results page

---
+++ Komposku/Module/CheckingResultsPage/CheckingResultsCell/CheckingResultsCell.swift
@@ -0,0 +1,23 @@
+//
+//  CheckingResultsCell.swift
+//  Komposku
+//
+//  Created by Adlan Nourindiaz on 03/08/21.
+//
+
+import UIKit
+
+class CheckingResultsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
