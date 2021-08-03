252f59a4a0d2773e7e96d462ed789fb21424d7fb
Ahmad Zaky W <a.zaky.wisnumurti@mail.ugm.ac.id>
2021-08-03 23:04:16 +0700

    Add String Modifier and Detail Material Cell

---
+++ Komposku/Core/Utils/Helper/StringModifier.swift
@@ -0,0 +1,8 @@
+//
+//  StringModifier.swift
+//  Komposku
+//
+//  Created by Ahmad Zaky on 03/08/21.
+//
+
+import Foundation
---
+++ Komposku/Module/CreateCompost/DetailMaterialCell/DetailMaterialTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailMaterialTableViewCell.swift
+//  Komposku
+//
+//  Created by Ahmad Zaky on 03/08/21.
+//
+
+import UIKit
+
+class DetailMaterialTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
